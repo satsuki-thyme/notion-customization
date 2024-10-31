4ce856f40200e73760aa7b88eb2e72ccba54d37c
satsuki-thyme <satsuki.thyme@gmail.com>
2024-11-01 03:06:53 +0900

    選択範囲を数えられるようにした

--- countWord.js
+++ countWord.js
@@ -4,25 +4,73 @@
 
 */
 (() => {
+  let shortcutKey = [`Alt`, `Shift`, `C`]
   document.querySelector(`body`).innerHTML += `<div class="count-word">Count</button>`
-  let countWordButton = document.querySelector(`.count-word`)
-  countWordButton.onclick = () => {
-    let contentsWrapper = document
-    .querySelector(`.notion-peek-renderer .notion-page-content`)
-    if (contentsWrapper) {
-      alert(countWord(contentsWrapper))
+  let countButton = document.querySelector(`.count-word`)
+  let keydown = {}
+  window.onkeydown = e => {
+    keydown[e.key] = true
+    if (
+      shortcutKeyDown()
+      &&
+      !otherKeyDown()
+    ) {
+      counterBody()
+      keydown.Alt = false
+      keydown.Shift = false
+      keydown.C = false
     }
-    else {
-      alert(countWord(document.querySelector(`.layout .notion-page-content`)))
+    function otherKeyDown() {
+      w = false
+      Object.keys(keydown)
+      .filter(e => keydown[e])
+      .filter(e => !shortcutKey.includes(e))
+      .forEach(e => {
+        if(keydown[e]) {
+          w = true
+        }
+      })
+      return w
     }
   }
-  function countWord(elem) {
-    let count = 0
-    elem
-    .querySelectorAll(`.notranslate`)
-    .forEach(e => {
-      count += e.textContent.replace(/\r?\n/g, ``).length
-    })
-    return count
+  window.onkeyup = e => {
+    keydown[e.key] = false
+  }
+  countButton.onclick = () => {
+    counterBody()
+  }
+  function shortcutKeyDown() {
+    let w = Object.keys(keydown)
+    .filter(e => keydown[e])
+    .filter(e => shortcutKey.includes(e))
+    return w.length === shortcutKey.length ? true : false
+  }
+  function counterBody() {
+    let selection = window.getSelection()
+    if (selection.type === `Caret`) {
+      let contentsWrapper = document
+      .querySelector(`.notion-peek-renderer .notion-page-content`)
+      if (contentsWrapper) {
+        alert(count(contentsWrapper))
+      }
+      else {
+        alert(count(document.querySelector(`.layout .notion-page-content`)))
+      }
+    }
+    if (selection.type === `Range`) {
+      alert(selection.toString().replace(/[\r\n]/g, ``).length)
+    }
+    if (selection.type === `None`) {
+      alert(`The selected paragraph/block itself is not supported.`)
+    }
+    function count(elem) {
+      let count = 0
+      elem
+      .querySelectorAll(`.notranslate`)
+      .forEach(e => {
+        count += e.textContent.replace(/\r?\n/g, ``).length
+      })
+      return count
+    }
   }
 })()
